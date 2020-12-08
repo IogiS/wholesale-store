fdcb3027adbec7c30f40f1d9c1b16c682515060a
IogiS <31819845+IogiS@users.noreply.github.com>
2020-12-08 13:39:30 +0300

    add project store

---
+++ wholesale-store/wholesale-store/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wholesale_store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
